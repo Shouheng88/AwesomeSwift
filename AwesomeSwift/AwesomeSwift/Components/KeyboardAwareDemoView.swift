df880c78ac5616ac481ac1e4cd116256011e5d7f
wangshouheng <wangshouheng@corp.netease.com>
2022-11-08 17:49:24 +0800

    feat: add chat input sample

---
+++ AwesomeSwift/AwesomeSwift/Components/KeyboardAwareDemoView.swift
@@ -0,0 +1,20 @@
+//
+//  KeyboardAwareDemoView.swift
+//  AwesomeSwift
+//
+//  Created by 王守恒 on 2022/11/6.
+//
+
+import SwiftUI
+
+struct KeyboardAwareDemoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct KeyboardAwareDemoView_Previews: PreviewProvider {
+    static var previews: some View {
+        KeyboardAwareDemoView()
+    }
+}
---
+++ AwesomeSwift/AwesomeSwift/Components/KeyboardAwareDemoView2.swift
@@ -0,0 +1,20 @@
+//
+//  KeyboardAwareDemoView2.swift
+//  AwesomeSwift
+//
+//  Created by 王守恒 on 2022/11/6.
+//
+
+import SwiftUI
+
+struct KeyboardAwareDemoView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct KeyboardAwareDemoView2_Previews: PreviewProvider {
+    static var previews: some View {
+        KeyboardAwareDemoView2()
+    }
+}
