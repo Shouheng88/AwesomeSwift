b8cda729977c9473e968b55f3f80790deccff22f
wangshouheng <wangshouheng@corp.netease.com>
2022-11-08 17:53:46 +0800

    feat: change chat input for versions

--- AwesomeSwift/AwesomeSwift/Components/KeyboardAwareDemoView.swift
+++ AwesomeSwift/AwesomeSwift/Components/KeyboardAwareDemoView.swift
@@ -7,10 +7,164 @@
 
 import SwiftUI
 
+
 struct KeyboardAwareDemoView: View {
+    
+    @State private var text = ""
+    @State var messages: [Message] = (0...60).map { Message(text: "message:\($0)") }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { proxy in
+            VStack(spacing: 0) {
+                List {
+                    ForEach(messages) { message in
+                        TextField(message.text, text: self.$text)
+                    }
+                }
+                if #available(iOS 15.0, *) {
+                    SendAbove15(proxy: proxy)
+                } else {
+                    Send(proxy: proxy)
+                }
+            }
+        }.navigationBarTitleDisplayMode(.inline)
     }
+    
+    @available(iOS 15.0, *)
+    private struct SendAbove15: View {
+        @State private var text: String = ""
+        @FocusState var keyboardFcused: Bool
+        @State private var showCalendar: Bool = false
+        @State private var keyboardHeight: CGFloat = 0.0
+
+        let proxy: GeometryProxy
+    
+        var body: some View {
+            VStack(spacing: 0) {
+                Divider()
+                ZStack {
+                    // The Color will extand the layout of ZStack too large.
+//                    Color(R.color.custom_color()!)
+                    VStack {
+                        HStack(spacing: 12) {
+                            Image(systemName: "calendar.circle").onTapGesture {
+                                // Get keyboard height.
+                                keyboardHeight = proxy.safeAreaInsets.bottom
+                                showCalendar = true
+                            }
+                            TextEditor(text: $text)
+                                .cornerRadius(4)
+                                .focused($keyboardFcused)
+                                .border(.gray)
+                            Image(systemName: "tray.circle")
+                            Image(systemName: "paperplane").onTapGesture {
+                                keyboardFcused = false
+                            }
+                        }
+                        .padding(.horizontal, 12)
+                        .frame(height: keyboardFcused ? 100 : 56)
+                        Text("Calendar Container")
+                            .frame(height: showCalendar && !keyboardFcused ? keyboardHeight : 0)
+                    }
+                }
+            }
+        }
+    }
+    
+    private struct Send: View, EventReceiver {
+
+        @State private var text: String = ""
+        @State private var showCalendar: Bool = false
+        @State private var keyboardFcused: Bool = false
+        private let keboardEventReceiver = KeboardEventReceiver()
+
+        let proxy: GeometryProxy
+                
+        var body: some View {
+            VStack(spacing: 0) {
+                Divider()
+                ZStack {
+                    VStack {
+                        HStack(spacing: 12) {
+                            Image(systemName: "calendar.circle").onTapGesture {
+                                
+                            }
+                            TextEditor(text: $text)
+                                .cornerRadius(4)
+                                .border(.gray)
+                            Image(systemName: "tray.circle")
+                            Image(systemName: "paperplane").onTapGesture {
+                                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                            }
+                        }
+                        .padding(.horizontal, 12)
+                        .frame(height: keyboardFcused ? 100 : 56)
+                    }
+                }
+            }.onAppear(perform: {
+                keboardEventReceiver.registerReceiver(receiver: self)
+                keboardEventReceiver.registerForKeyboardNotifications()
+            }).onDisappear(perform: {
+                keboardEventReceiver.unRegisterForKeyboardNotifications()
+            })
+        }
+        
+        func onReceive(hide: Bool) {
+            self.keyboardFcused = !hide
+        }
+    }
+}
+
+/// Keyboard show and hide event detector.
+class KeboardEventReceiver {
+    
+    private var receivers: [EventReceiver] = []
+
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShown(_:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillBeHidden(_:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
+    }
+    
+    func unRegisterForKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(
+            self,
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+        NotificationCenter.default.removeObserver(
+            self,
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
+    }
+    
+    func registerReceiver(receiver: EventReceiver) {
+        receivers.append(receiver)
+    }
+    
+    @objc func keyboardWillShown(_ notificiation: NSNotification) {
+        debugPrint("keyboard shown")
+        receivers.forEach({ receiver in
+            receiver.onReceive(hide: false)
+        })
+    }
+    
+    @objc func keyboardWillBeHidden(_ notification: NSNotification) {
+        debugPrint("keyboard hidden")
+        receivers.forEach({ receiver in
+            receiver.onReceive(hide: true)
+        })
+    }
+}
+
+protocol EventReceiver {
+    func onReceive(hide: Bool)
 }
 
 struct KeyboardAwareDemoView_Previews: PreviewProvider {
--- AwesomeSwift/AwesomeSwift/Components/KeyboardAwareDemoView2.swift
+++ AwesomeSwift/AwesomeSwift/Components/KeyboardAwareDemoView2.swift
@@ -7,14 +7,85 @@
 
 import SwiftUI
 
+@available(iOS 15.0, *)
 struct KeyboardAwareDemoView2: View {
+
+    @State private var text = ""
+    @State var messages: [Message] = (0...60).map { Message(text: "message:\($0)") }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { proxy in
+            VStack(spacing: 0) {
+                List {
+                    ForEach(messages) { message in
+                        TextField(text: self.$text, label: {
+                            Text(message.text).id(message.id)
+                        })
+                    }
+                }
+                .listStyle(.inset)
+                .safeAreaInset(edge: .bottom) {
+                    SendEdge(messages: $messages)
+                }
+            }
+        }.navigationBarTitleDisplayMode(.inline)
     }
+    
+    private struct SendEdge: View {
+
+        @Binding var messages: [Message]
+        @FocusState var focused: Bool
+        @State private var text = ""
+
+        var body: some View {
+            ZStack(alignment: .top) {
+                Color.clear
+                Rectangle()
+                    .fill(.secondary)
+                    .opacity(0.3).frame(height: 0.6) // 上部线条
+                HStack(alignment: .firstTextBaseline) {
+                    // 输入框
+                    TextField("", text: $text)
+//                    TextField(text: $text)
+                        .focused($focused)
+                        .textFieldStyle(.roundedBorder)
+                        .padding(.horizontal, 10)
+                        .padding(.top, 10)
+                        .onSubmit {
+                            debugPrint("commited")
+                            messages.append(Message(text: text))
+                        }
+                        .onChange(of: focused) { value in
+                            
+                        }
+                    // 回复按钮
+                    Button("回复") {
+                        focused = false
+                        messages.append(Message(text: text))
+                    }
+                    .buttonStyle(.bordered)
+                    .controlSize(.small)
+                    .tint(.green)
+                }
+                .padding(.horizontal, 30)
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 53)
+            .background(.regularMaterial)
+        }
+    }
+}
+
+struct Message: Identifiable, Hashable {
+    let id = UUID()
+    let text: String
 }
 
 struct KeyboardAwareDemoView2_Previews: PreviewProvider {
     static var previews: some View {
-        KeyboardAwareDemoView2()
+        if #available(iOS 15.0, *) {
+            KeyboardAwareDemoView2()
+        } else {
+            Text("Only available on 15.0 and newer")
+        }
     }
 }
