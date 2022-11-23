cde179eec920a8869a733dae235e88ea09792786
wangshouheng <wangshouheng@corp.netease.com>
2022-11-23 23:38:00 +0800

    feat: add review

--- AwesomeSwift/AwesomeSwift/Components/LaunchPhonePageDemo.swift
+++ AwesomeSwift/AwesomeSwift/Components/LaunchPhonePageDemo.swift
@@ -6,15 +6,46 @@
 //
 
 import SwiftUI
+import StoreKit
 
+//@available(iOS 16.0, *)
 struct LaunchPhonePageDemo: View {
+    
+//    @Environment(\.requestReview) var requestReview
+    
+    @AppStorage("runsSinceLastRequest") static var runsSinceLastRequest = 0
+    @AppStorage("version") static var version = ""
+    static var limit = 10
+
+    private var showAppReviewDemo: some View {
+        VStack {
+            Text("Review runsSinceLastRequest [\(LaunchPhonePageDemo.runsSinceLastRequest)]")
+            Text("Review version [\(LaunchPhonePageDemo.version)]")
+
+            Button("Request Review (before 16.0)", action: {
+                if let scene = UIApplication.shared.connectedScenes.first(where: {$0.activationState == .foregroundActive}) as? UIWindowScene {
+                    SKStoreReviewController.requestReview(in: scene)
+                }
+            }).frame(height: 40)
+            
+            Button("Request Review (before 16.0)", action: {
+                LaunchPhonePageDemo.showReview()
+            }).frame(height: 40)
+            
+            Button("Request Review (16.0 and above)", action: {
+//                requestReview()
+                // Use this method on 16.0 and above.
+            }).frame(height: 40)
+        }
+    }
+    
     var body: some View {
         VStack {
-            Text("Open App Settings")
-                .frame(height: 40)
-                .onTapGesture {
-                    openAppSpecificSettings()
-                }
+            Button("Open App Settings", action: {
+                openAppSpecificSettings()
+            }).frame(height: 40)
+            
+            showAppReviewDemo
         }
     }
     
@@ -27,10 +58,36 @@ struct LaunchPhonePageDemo: View {
         let optionsKeyDictionary = [UIApplication.OpenExternalURLOptionsKey(rawValue: "universalLinksOnly"): NSNumber(value: true)]
         UIApplication.shared.open(url, options: optionsKeyDictionary, completionHandler: nil)
     }
+    
+    static func showReview() {
+        runsSinceLastRequest += 1
+        let appBuild = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String
+        let appVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+        let currentVersion = "Version \(appVersion), build \(appBuild)"
+
+        guard currentVersion != version else {
+            runsSinceLastRequest = 0
+            return
+        }
+
+        guard runsSinceLastRequest == limit else { return }
+
+        if let scene = UIApplication.shared.connectedScenes.first(where: {$0.activationState == .foregroundActive}) as? UIWindowScene {
+            SKStoreReviewController.requestReview(in: scene)
+            // Reset runsSinceLastRequest
+            runsSinceLastRequest = 0
+            // Set version to currentVersion
+            version = currentVersion
+        }
+    }
 }
 
 struct LaunchPhonePageDemo_Previews: PreviewProvider {
     static var previews: some View {
-        LaunchPhonePageDemo()
+        if #available(iOS 16.0, *) {
+            LaunchPhonePageDemo()
+        } else {
+            EmptyView()
+        }
     }
 }
