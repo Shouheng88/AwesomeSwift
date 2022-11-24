6abc4ffd4a1ebc1ced9cbb62ff690992f540a608
wangshouheng <wangshouheng@corp.netease.com>
2022-11-24 16:34:50 +0800

    feat: add email sample

---
+++ AwesomeSwift/AwesomeSwift/Components/Business/MailView.swift
@@ -0,0 +1,61 @@
+//
+//  MailView.swift
+//  AwesomeSwift
+//
+//  Created by 王守恒 on 2022/11/23.
+//
+
+import SwiftUI
+import UIKit
+import MessageUI
+
+struct MailView: UIViewControllerRepresentable {
+
+    @Environment(\.presentationMode) var presentation
+    @Binding var result: Result<MFMailComposeResult, Error>?
+
+    class Coordinator: NSObject, MFMailComposeViewControllerDelegate {
+
+        @Binding var presentation: PresentationMode
+        @Binding var result: Result<MFMailComposeResult, Error>?
+
+        init(
+            presentation: Binding<PresentationMode>,
+            result: Binding<Result<MFMailComposeResult, Error>?>
+        ) {
+            _presentation = presentation
+            _result = result
+        }
+
+        func mailComposeController(
+            _ controller: MFMailComposeViewController,
+            didFinishWith result: MFMailComposeResult,
+            error: Error?
+        ) {
+            defer {
+                $presentation.wrappedValue.dismiss()
+            }
+            guard error == nil else {
+                self.result = .failure(error!)
+                return
+            }
+            self.result = .success(result)
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(presentation: presentation, result: $result)
+    }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<MailView>) -> MFMailComposeViewController {
+        let vc = MFMailComposeViewController()
+        vc.mailComposeDelegate = context.coordinator
+        return vc
+    }
+
+    func updateUIViewController(
+        _ uiViewController: MFMailComposeViewController,
+        context: UIViewControllerRepresentableContext<MailView>
+    ) {
+    }
+}
--- AwesomeSwift/AwesomeSwift/Components/LaunchPhonePageDemo.swift
+++ AwesomeSwift/AwesomeSwift/Components/LaunchPhonePageDemo.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import StoreKit
+import MessageUI
 
 //@available(iOS 16.0, *)
 struct LaunchPhonePageDemo: View {
@@ -17,6 +18,11 @@ struct LaunchPhonePageDemo: View {
     @AppStorage("version") static var version = ""
     static var limit = 10
 
+    @State var result: Result<MFMailComposeResult, Error>? = nil
+    @State var isShowingMailView = false
+
+    @State private var emailState: String = ""
+    
     private var showAppReviewDemo: some View {
         VStack {
             Text("Review runsSinceLastRequest [\(LaunchPhonePageDemo.runsSinceLastRequest)]")
@@ -36,6 +42,22 @@ struct LaunchPhonePageDemo: View {
 //                requestReview()
                 // Use this method on 16.0 and above.
             }).frame(height: 40)
+            
+            Button(action: {
+               self.isShowingMailView.toggle()
+            }) {
+                Text("Send Email")
+            }
+            .frame(height: 40)
+            .disabled(!MFMailComposeViewController.canSendMail())
+            .sheet(isPresented: $isShowingMailView) {
+                MailView(result: self.$result)
+            }
+            
+            Button("Send Email by URL", action: {
+                sendEmailByUrl()
+            }).frame(height: 40)
+            Text(emailState)
         }
     }
     
@@ -46,6 +68,33 @@ struct LaunchPhonePageDemo: View {
             }).frame(height: 40)
             
             showAppReviewDemo
+        }
+    }
+    
+    private func sendEmailByUrl() {
+        let subject = "Sample Email Subject"
+        let body = "Sample message body"
+        let mailto = "mailto:shouheng2015@gmail.com?subject=\(subject)&body=\(body)"
+
+        guard let escapedMailto = mailto.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            self.emailState = "email addingPercentEncoding failed"
+            return
+        }
+        guard let url = URL(string: escapedMailto) else {
+            self.emailState = "email url encode failed"
+            return
+        }
+
+        guard UIApplication.shared.canOpenURL(url) else {
+            self.emailState = "canOpenURL: Unable to determine email sending state"
+            return
+        }
+
+        UIApplication.shared.open(url, options: [.universalLinksOnly : false]) { (success) in
+            // Handle success/failure
+            if !success {
+                self.emailState = "Unable to determine email sending state"
+            }
         }
     }
     
