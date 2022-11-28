4b18e51714a5702778d45901a97b536451371c48
wangshouheng <wangshouheng@corp.netease.com>
2022-11-28 17:26:00 +0800

    feat: file options

--- AwesomeSwift/AwesomeSwift/Components/Business/Storage/FileDemoView.swift
+++ AwesomeSwift/AwesomeSwift/Components/Business/Storage/FileDemoView.swift
@@ -19,6 +19,12 @@ struct FileDemoView: View {
         return "- " + "\n- ".join(list: urls)
     }
     
+    // 深度遍历指定目录
+    private var contentsOfDocumentDirectoryDepth: String {
+        let paths = FileManager.default.enumerator(atPath: documentDirectoryUrl.path)
+        return "- " + "\n- ".join(list: paths?.allObjects ?? [])
+    }
+    
     private var directoryInfoView: some View {
         VStack(alignment: .leading, spacing: 8) {
             DirectoryInfoView(message: ".documentDirectory:\n \(urlFor(for: .documentDirectory))")
@@ -30,6 +36,7 @@ struct FileDemoView: View {
             DirectoryInfoView(message: ".trashDirectory:\n \(urlFor(for: .trashDirectory))")
             DirectoryInfoView(message: ".applicationSupportDirectory:\n \(urlFor(for: .applicationSupportDirectory))")
             DirectoryInfoView(message: ".documentDirectory content:\n \(contentsOfDocumentDirectory)")
+            DirectoryInfoView(message: "contentsOfDocumentDirectoryDepth:\n \(contentsOfDocumentDirectoryDepth)")
         }.onAppear(perform: {
             // 另外一种查看模拟器里的目录的方式是，将以下打印出的内容在 Finder 中选择 “前往->前往文件夹”，输入，然后可以进入对应目录
             print(urlFor(for: .documentDirectory))
@@ -38,9 +45,19 @@ struct FileDemoView: View {
     
     private var fileOptionsView: some View {
         VStack {
-            Button("", action: {
-                
-            })
+            Text("Sample text file exists: \(String(sampleFileExists()))").frame(height: 40)
+            Button("Write Text To File", action: {
+                writeTextToFile()
+            }).frame(height: 40)
+            Button("Write Image To File", action: {
+                writeImageToFile()
+            }).frame(height: 40)
+            Button("Write Data To File", action: {
+                writeDataToFile()
+            }).frame(height: 40)
+            Button("Copy File", action: {
+                copyFile()
+            }).frame(height: 40)
         }
     }
     
@@ -71,6 +88,7 @@ struct FileDemoView: View {
             let fm = FileManager.default
             let docsUrl = try fm.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
             let folder = docsUrl.appendingPathComponent("test_folder").appendingPathComponent("test_folder")
+            // withIntermediateDirectories 表示如果路径的中间路径不存在，则创建它
             try fm.createDirectory(at: folder, withIntermediateDirectories: true)
         } catch {
             print("failed to create folder")
@@ -86,6 +104,54 @@ struct FileDemoView: View {
         } catch {
             print("failed to create folder")
         }
+    }
+    
+    private func writeTextToFile() {
+        do {
+            let textUrl = documentDirectoryUrl.appendingPathComponent("sample.txt")
+            try "sample sample sample".write(toFile: textUrl.path, atomically: true, encoding: .utf8)
+        } catch {
+            print("failed to write text to file")
+        }
+    }
+    
+    private func writeImageToFile() {
+        do {
+            let data = R.image.the_soup()?.jpegData(compressionQuality: 1)
+            let url = documentDirectoryUrl.appendingPathComponent("image.jpg")
+            try data?.write(to: url)
+        } catch {
+            print("failed to write image to file")
+        }
+    }
+    
+    private func writeDataToFile() {
+        let data = "asdadasdsadsa".data(using: .utf8)
+        let url = documentDirectoryUrl.appendingPathComponent("data")
+        FileManager.default.createFile(atPath: url.path, contents: data)
+    }
+    
+    private func copyFile() {
+        do {
+            let copyFrom = documentDirectoryUrl.appendingPathComponent("sample.txt")
+            let directory = documentDirectoryUrl.appendingPathComponent("test")
+            if !FileManager.default.fileExists(atPath: directory.path) {
+                try FileManager.default.createDirectory(at: directory, withIntermediateDirectories: true)
+            }
+            let copyTo = directory.appendingPathComponent("copied_sample.txt")
+            if !FileManager.default.fileExists(atPath: copyTo.path) {
+                try FileManager.default.copyItem(at: copyFrom, to: copyTo)
+            } else {
+                print("file exists")
+            }
+        } catch {
+            print("failed to copy file")
+        }
+    }
+    
+    private func sampleFileExists() -> Bool {
+        let textUrl = documentDirectoryUrl.appendingPathComponent("sample.txt")
+        return FileManager.default.fileExists(atPath: textUrl.path)
     }
     
     var body: some View {
