68dff15e38ad8054b062dd7fdfd0f15bf2ebf6e9
wangshouheng <wangshouheng@corp.netease.com>
2022-12-03 15:29:12 +0800

    feat: add new alert demo

--- AwesomeSwift/AwesomeSwift/Components/Business/AlertDemoView.swift
+++ AwesomeSwift/AwesomeSwift/Components/Business/AlertDemoView.swift
@@ -16,30 +16,45 @@ struct AlertDemoView: View {
     @State private var completeToast: Bool = false
     @State private var loadingToast: Bool = false
     @State private var systemImageToast: Bool = false
+    @State private var systemImageBannerPop: Bool = false
+    @State private var systemImageBannerSlide: Bool = false
+    @State private var systemImageHub: Bool = false
 
     var body: some View {
         VStack {
-            Button("Toast .regular", action: {
-                self.regularToast.toggle()
-            }).frame(height: 40)
-            Button("Toast .regular long", action: {
-                self.regularToastLong.toggle()
-            }).frame(height: 40)
-            Button("Toast .regular long subtitle", action: {
-                self.regularToastLongSubTitle.toggle()
-            }).frame(height: 40)
-            Button("Toast .error", action: {
-                self.errorToast.toggle()
-            }).frame(height: 40)
-            Button("Toast .complete", action: {
-                self.completeToast.toggle()
-            }).frame(height: 40)
-            Button("Toast .loading", action: {
-                self.loadingToast.toggle()
-            }).frame(height: 40)
-            Button("Toast .systemImage", action: {
-                self.systemImageToast.toggle()
-            }).frame(height: 40)
+            Spacer()
+            Group {
+                Button("Toast .regular", action: {
+                    self.regularToast.toggle()
+                }).frame(height: 40)
+                Button("Toast .regular long", action: {
+                    self.regularToastLong.toggle()
+                }).frame(height: 40)
+                Button("Toast .regular long subtitle", action: {
+                    self.regularToastLongSubTitle.toggle()
+                }).frame(height: 40)
+                Button("Toast .error", action: {
+                    self.errorToast.toggle()
+                }).frame(height: 40)
+                Button("Toast .complete", action: {
+                    self.completeToast.toggle()
+                }).frame(height: 40)
+                Button("Toast .loading", action: {
+                    self.loadingToast.toggle()
+                }).frame(height: 40)
+                Button("Toast .systemImage", action: {
+                    self.systemImageToast.toggle()
+                }).frame(height: 40)
+                Button("Toast .systemImage banner pop", action: {
+                    self.systemImageBannerPop.toggle()
+                }).frame(height: 40)
+                Button("Toast .systemImage banner slide", action: {
+                    self.systemImageBannerSlide.toggle()
+                }).frame(height: 40)
+                Button("Toast .systemImage hub", action: {
+                    self.systemImageHub.toggle()
+                }).frame(height: 40)
+            }
         }
         .toast(isPresenting: $regularToast){
             AlertToast(type: .regular, title: "alert_title_short")
@@ -62,6 +77,16 @@ struct AlertDemoView: View {
         .toast(isPresenting: $systemImageToast, alert: {
             AlertToast(type: .systemImage("scribble", Color.green), title: "alert_title_short", subTitle: "alert_title_long")
         })
+        .toast(isPresenting: $systemImageBannerPop, alert: {
+            AlertToast(displayMode: .banner(.pop), type: .systemImage("scribble", Color.green), title: "alert_title_short", subTitle: "alert_title_long")
+        })
+        .toast(isPresenting: $systemImageBannerSlide, alert: {
+            AlertToast(displayMode: .banner(.slide), type: .systemImage("scribble", Color.green), title: "alert_title_short", subTitle: "alert_title_long")
+        })
+        .toast(isPresenting: $systemImageHub, alert: {
+            AlertToast(displayMode: .hud, type: .systemImage("scribble", Color.green), title: "alert_title_short", subTitle: "alert_title_long")
+        })
+//        .navigationBarHidden(true)
         .navigationTitle("Alert Demo")
             .navigationBarTitleDisplayMode(.inline)
     }
