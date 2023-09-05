0b8275f513ca35718b4c301e8e71fe8478a9a816
wangshouheng <shouheng2015@gmail.com>
2023-09-05 11:13:16 +0800

    rfct: image sample

---
+++ AwesomeSwift/AwesomeSwift/Components/Widget/ImageDemoView.swift
@@ -0,0 +1,180 @@
+//
+//  ImageDemoView.swift
+//  swiftdemo (iOS)
+//
+//  Created by 王守恒 on 2022/9/14.
+//
+
+import Rswift
+import SwiftUI
+
+/// 图片示例
+struct ImageDemoView: View {
+
+    @State private var tip: String = ""
+    @State private var playLottie: Int = 0
+
+    private var sampleResourceImageView: some View {
+        VStack {
+            SampleSectionTitleView(title: "1. 资源图片示例")
+            HStack {
+                Image(uiImage: R.image.heart_lock()!)
+                    .onTapGesture {
+                        self.tip = "点击了图片"
+                    }
+                Image(uiImage: R.image.heart_lock()!)
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .onTapGesture {
+                        self.tip = "点击了 30x 图片"
+                    }
+                Image(uiImage: R.image.heart_lock()!)
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .onTapGesture {
+                        self.tip = "点击了 50x 图片"
+                    }.blur(radius: 3)
+            }
+        }
+    }
+    
+    private var sampleGradientImageView: some View {
+        VStack {
+            SampleSectionTitleView(title: "2. 渐变图片 Shape 等")
+            ScrollView(.horizontal) {
+                HStack {
+                    LinearGradient(
+                        colors: [.red, .green, .blue],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    ).frame(width: 100, height: 100)
+                        .overlay(Text("LinearGradient").foregroundColor(.white))
+                    RoundedRectangle(cornerRadius: 10.0)
+                        .fill(.blue)
+                        .frame(width: 100, height: 100)
+                        .overlay(Text("RoundedRectangle").foregroundColor(.white))
+                    Capsule().fill(.red)
+                        .frame(width: 150, height: 100)
+                        .overlay(Text("Capsule").foregroundColor(.white))
+                    Circle().fill(.green)
+                        .frame(width: 100, height: 100)
+                        .overlay(Text("Circle").foregroundColor(.white))
+                }
+            }
+        }
+    }
+    
+    private var sampleImageEffectView: some View {
+        VStack {
+            SampleSectionTitleView(title: "3. 图片效果")
+            ScrollView(.horizontal) {
+                HStack {
+                    Image(uiImage: R.image.the_soup()!)
+                        .resizable()
+                        .clipShape(Circle())
+                        .frame(width: 100, height: 100)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 100)
+                                .stroke(.blue, lineWidth: 5)
+                                .shadow(color: .gray, radius: 2, x: 2, y: 2)
+                        )
+                    Image(uiImage: R.image.the_soup()!)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 100)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 100)
+                                .stroke(.blue, lineWidth: 2)
+                                .shadow(color: .gray, radius: 2, x: 2, y: 2)
+                        )
+                    Image(uiImage: R.image.the_soup()!)
+                        .resizable()
+                        .scaleEffect()
+                        .frame(width: 100, height: 100)
+                    Image(uiImage: R.image.the_soup()!)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100, alignment: .top)
+                        .overlay(Text("Top alignment"))
+//                        .overlay(content: { // This method is only available on 16.0 and newer
+//                            Text("Top alignment")
+//                        })
+                    Image(uiImage: R.image.the_soup()!)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100, alignment: .center)
+                        .overlay(Text("Center alignment"))
+                    Image(uiImage: R.image.the_soup()!)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100, alignment: .bottom)
+                        .overlay(Text("Bottom alignment"))
+                    Image(uiImage: R.image.the_soup()!)
+                        .resizable()
+                        .clipShape(ContainerRelativeShape())
+                        .frame(width: 100, height: 100)
+                    Image(uiImage: R.image.the_soup()!)
+                        .resizable()
+                        .clipShape(Circle())
+                        .frame(width: 100, height: 100)
+                    Image(uiImage: R.image.the_soup()!)
+                        .resizable()
+                        .clipShape(Ellipse())
+                        .frame(width: 100, height: 100)
+                    Image(uiImage: R.image.the_soup()!)
+                        .resizable()
+                        .clipShape(Capsule())
+                        .frame(width: 100, height: 100)
+                }
+            }
+        }
+    }
+    
+    private var sampleSystemImageView: some View {
+        VStack {
+            SampleSectionTitleView(title: "4. 系统内置图片")
+            HStack {
+                Image(systemName: "3.circle").imageScale(.large)
+                Image(systemName: "13.circle").imageScale(.large)
+                Image(systemName: "50.circle").imageScale(.large) // max
+            }
+        }
+    }
+    
+    private var sampleLottieImageView: some View {
+        VStack {
+            SampleSectionTitleView(title: "5. Lottie 图片")
+            LottieView(lottieFile: "thank_you", play: self.$playLottie)
+                .frame(width: 200, height: 200)
+                .onTapGesture {
+                    self.playLottie = self.playLottie + 1
+                }
+        }
+    }
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Text("\(self.tip)").foregroundColor(.gray).font(.system(size: 14))
+                sampleResourceImageView
+                sampleGradientImageView
+                sampleImageEffectView
+                sampleSystemImageView
+                sampleLottieImageView
+            }.padding(15)
+        }.navigationTitle(Text("图片示例"))
+            .navigationBarHidden(false)
+            .navigationBarItems(
+                leading: Text("头部"),
+                trailing: Text("尾部").onTapGesture {
+                    self.tip = "点击了尾部"
+                }
+            )
+    }
+}
+
+struct ImageDemoView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageDemoView()
+    }
+}
