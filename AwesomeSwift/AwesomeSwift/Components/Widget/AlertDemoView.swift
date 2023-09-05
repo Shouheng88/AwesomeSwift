7b4c2b754a35959e464399d9242efa1b30e91bae
wangshouheng <shouheng2015@gmail.com>
2023-09-05 22:37:07 +0800

    rfct: aler demo

--- AwesomeSwift/AwesomeSwift/Components/Widget/AlertDemoView.swift
+++ AwesomeSwift/AwesomeSwift/Components/Widget/AlertDemoView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+/// 提示示例
 struct AlertDemoView: View {
     
     @State private var regularToast: Bool = false
@@ -22,72 +23,69 @@ struct AlertDemoView: View {
 
     var body: some View {
         VStack {
-            Spacer()
             Group {
-                Button("Toast .regular", action: {
+                Button("弹出 .regular 类型", action: {
                     self.regularToast.toggle()
                 }).frame(height: 40)
-                Button("Toast .regular long", action: {
+                Button("弹出 .regular 类型", action: {
                     self.regularToastLong.toggle()
                 }).frame(height: 40)
-                Button("Toast .regular long subtitle", action: {
+                Button("弹出 .regular 类型", action: {
                     self.regularToastLongSubTitle.toggle()
                 }).frame(height: 40)
-                Button("Toast .error", action: {
+                Button("弹出 .error 类型", action: {
                     self.errorToast.toggle()
                 }).frame(height: 40)
-                Button("Toast .complete", action: {
+                Button("弹出 .complete 类型", action: {
                     self.completeToast.toggle()
                 }).frame(height: 40)
-                Button("Toast .loading", action: {
+                Button("弹出 .loading 类型", action: {
                     self.loadingToast.toggle()
                 }).frame(height: 40)
-                Button("Toast .systemImage", action: {
+                Button("弹出 .systemImage 类型", action: {
                     self.systemImageToast.toggle()
                 }).frame(height: 40)
-                Button("Toast .systemImage banner pop", action: {
+                Button("弹出 .banner(.pop) 类型", action: {
                     self.systemImageBannerPop.toggle()
                 }).frame(height: 40)
-                Button("Toast .systemImage banner slide", action: {
+                Button("弹出 .banner(.slide) 类型", action: {
                     self.systemImageBannerSlide.toggle()
                 }).frame(height: 40)
-                Button("Toast .systemImage hub", action: {
+                Button("弹出 .hub 类型", action: {
                     self.systemImageHub.toggle()
                 }).frame(height: 40)
             }
         }
         .toast(isPresenting: $regularToast){
-            AlertToast(type: .regular, title: "alert_title_short")
+            AlertToast(type: .regular, title: "标题")
         }
         .toast(isPresenting: $regularToastLong){
-            AlertToast(type: .regular, title: "alert_title_long")
+            AlertToast(type: .regular, title: "标题")
         }
         .toast(isPresenting: $regularToastLongSubTitle){
-            AlertToast(type: .regular, subTitle: "alert_title_long")
+            AlertToast(type: .regular, subTitle: "副标题")
         }
         .toast(isPresenting: $errorToast, alert: {
-            AlertToast(type: .error(Color.yellow), title: "alert_title_short", subTitle: "alert_title_long")
+            AlertToast(type: .error(Color.red), title: "错误", subTitle: "副标题")
         })
         .toast(isPresenting: $completeToast, alert: {
-            AlertToast(type: .complete(Color.blue), title: "alert_title_short", subTitle: "alert_title_long")
+            AlertToast(type: .complete(Color.green), title: "已完成", subTitle: "副标题")
         })
         .toast(isPresenting: $loadingToast, alert: {
-            AlertToast(type: .loading, title: "alert_title_short", subTitle: "alert_title_long")
+            AlertToast(type: .loading, title: "标题", subTitle: "副标题")
         })
         .toast(isPresenting: $systemImageToast, alert: {
-            AlertToast(type: .systemImage("scribble", Color.green), title: "alert_title_short", subTitle: "alert_title_long")
+            AlertToast(type: .systemImage("scribble", Color.green), title: "标题", subTitle: "副标题")
         })
         .toast(isPresenting: $systemImageBannerPop, alert: {
-            AlertToast(displayMode: .banner(.pop), type: .systemImage("scribble", Color.green), title: "alert_title_short", subTitle: "alert_title_long")
+            AlertToast(displayMode: .banner(.pop), type: .systemImage("scribble", Color.green), title: "标题", subTitle: "副标题")
         })
         .toast(isPresenting: $systemImageBannerSlide, alert: {
-            AlertToast(displayMode: .banner(.slide), type: .systemImage("scribble", Color.green), title: "alert_title_short", subTitle: "alert_title_long")
+            AlertToast(displayMode: .banner(.slide), type: .systemImage("scribble", Color.green), title: "标题", subTitle: "副标题")
         })
         .toast(isPresenting: $systemImageHub, alert: {
-            AlertToast(displayMode: .hud, type: .systemImage("scribble", Color.green), title: "alert_title_short", subTitle: "alert_title_long")
+            AlertToast(displayMode: .hud, type: .systemImage("scribble", Color.green), title: "标题", subTitle: "副标题")
         })
-//        .navigationBarHidden(true)
-        .navigationTitle("Alert Demo")
-            .navigationBarTitleDisplayMode(.inline)
+        .navigationTitle("提示示例").navigationBarTitleDisplayMode(.inline)
     }
 }
